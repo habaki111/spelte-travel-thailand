79f9383dc6e33fc2b077ec078a774a3ab4bf606e
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-05-26 15:06:03 +0700

    ref: Navbar and footer

--- components/Navbar.php
+++ components/Navbar.php
@@ -1,53 +1,49 @@
 <header>
-        <h1>Travel Thailand</h1>
-    </header>
+    <h1>Travel Thailand</h1>
+</header>
 
-    <!-- เพิ่มส่วน Navbar นี้ -->
-    <nav class="navbar">
-        <ul>
-            <li><a href="#">หน้าแรก</a></li>
-            <li><a href="#">เกี่ยวกับเรา</a></li>
-            <li><a href="#">บริการของเรา</a></li>
-            <li><a href="#">ติดต่อเรา</a></li>
-        </ul>
-    </nav>
+<!-- เพิ่มส่วน Navbar นี้ -->
+<nav class="navbar">
+    <ul>
+        <li><a href="#">หน้าแรก</a></li>
+        <li><a href="#">เกี่ยวกับเรา</a></li>
+        <li><a href="#">บริการของเรา</a></li>
+        <li><a href="#">ติดต่อเรา</a></li>
+    </ul>
+</nav>
 
-    <div class="container">
-        <!-- เนื้อหาต่าง ๆ ของเว็บไซต์ -->
+<div class="container">
+    <!-- เนื้อหาต่าง ๆ ของเว็บไซต์ -->
 
-        <!-- เพิ่มส่วนต่อไปนี้ในแต่ละหน้าที่ต้องการแสดง Navbar -->
-    </div>
+    <!-- เพิ่มส่วนต่อไปนี้ในแต่ละหน้าที่ต้องการแสดง Navbar -->
+</div>
 
 <style>
     header {
-            background-color: #333;
-            color: #fff;
-            padding: 20px;
-            text-align: center;
-        }
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+        text-align: center;
+    }
 
-        nav {
-            background-color: #f4f4f4;
-            padding: 10px;
-        }
+    nav {
+        background-color: #f4f4f4;
+        padding: 10px;
+    }
 
-        nav ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-        }
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
 
-        nav ul li {
-            display: inline;
-        }
+    nav ul li {
+        display: inline;
+    }
 
-        nav ul li a {
-            color: #333;
-            text-decoration: none;
-            padding: 10px;
-        }
-       
+    nav ul li a {
+        color: #333;
+        text-decoration: none;
+        padding: 10px;
+    }
 </style>
-
-
-
--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,12 @@
 <footer>
         <p>สิทธิ์ต่อเนื่อง &copy; 2023 เว็บไซต์ตัวอย่าง</p>
-        
-        <style>
-                    footer {
-            background-color: #333;
-            color: #fff;
-            padding: 20px;
-            text-align: center;
+</footer>
+
+<style>
+        footer {
+                background-color: #333;
+                color: #fff;
+                padding: 20px;
+                text-align: center;
         }
-        </style>
+</style>
